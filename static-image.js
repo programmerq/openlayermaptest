fd7b8f04ba4cc2a28200811869c69b744c49ba2f
Jeff Anderson <jefferya@programmerq.net>
2015-04-27 21:12:56 -0600

    very early semi-working two layer POC

---
+++ static-image.js
@@ -0,0 +1,53 @@
+// Map views always need a projection.  Here we just want to map image
+// coordinates directly to map coordinates, so we create a projection that uses
+// the image extent in pixels.
+var extent = [0, 0, 4875, 3225];
+var projection = new ol.proj.Projection({
+    code: 'eberron-image',
+    units: 'pixels',
+    extent: extent
+});
+var throneholdextent = [2302, 1564+1.75*35, 2302+38, 1564+2.75*35];
+var throneholdprojection = new ol.proj.Projection({
+    code: 'thronehold-image',
+    units: 'pixels',
+    extent: throneholdextent
+});
+
+var map = new ol.Map({
+    layers: [
+        new ol.layer.Image({
+            source: new ol.source.ImageStatic({
+                attributions: [
+                    new ol.Attribution({
+                        html: 'Khorvaire'
+                    })
+                ],
+                url: 'Khorvaire 4th edition.jpg',
+                imageSize: [4575, 3225],
+                projection: projection,
+                imageExtent: extent
+            })
+        }),
+        new ol.layer.Image({
+            source: new ol.source.ImageStatic({
+                attributions: [
+                    new ol.Attribution({
+                        html: 'Khorvaire'
+                    })
+                ],
+                url: 'Thronehold.jpg',
+                imageSize: [950, 875],
+                projection: projection,
+                imageExtent: throneholdextent,
+                maxResolution: 0.2
+            })
+        })
+    ],
+    target: 'map',
+    view: new ol.View({
+        projection: projection,
+        center: ol.extent.getCenter(extent),
+        zoom: 2
+    })
+});
